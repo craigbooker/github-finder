f5690158c1f539c7539cc797c66597502ae11594
Craig Booker <craig@arcadiasoftworks.com>
2020-03-05 21:58:50 -0600

    Moved repo state to the github context.

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -36,10 +36,40 @@ const GithubState = props => {
 	};
 
 	// Get User
+	// Get single Github user
+	const getUser = async username => {
+		setLoading();
+
+		//console.log(text);
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		dispatch({
+			type: GET_USER,
+			payload: res.data
+		});
+	};
 
 	// Get Repos
+	// Get users repos
+	const getUserRepos = async username => {
+		setLoading();
+
+		//console.log(text);
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		dispatch({
+			type: GET_REPOS,
+			payload: res.data
+		});
+	};
 
 	// Clear Users
+	// Clear Users from state
+	const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
 	// Set Loading
 	const setLoading = () => {
@@ -53,7 +83,10 @@ const GithubState = props => {
 				user: state.user,
 				repos: state.repos,
 				loading: state.loading,
-				searchUsers
+				searchUsers,
+				clearUsers,
+				getUser,
+				getUserRepos
 			}}
 		>
 			{props.children}
